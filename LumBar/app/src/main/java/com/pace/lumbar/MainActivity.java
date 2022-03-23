e4b47bf9b8b0b7ea8b79390582a8c639f87ecc05
ce60939n <ce60939n@pace.edu>
2022-03-23 08:32:52 -0400

    first Lumbar commit

---
+++ LumBar/app/src/main/java/com/pace/lumbar/MainActivity.java
@@ -0,0 +1,28 @@
+package com.pace.lumbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void likePressed(View view) {
+        Toast toast = Toast.makeText(this, R.string.like_message,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void dislikePressed(View view) {
+        Toast toast = Toast.makeText(this, R.string.dislike_message,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
