da972790f2bf8361d2c0474567a693b8c395b15e
josephine chan <jc41405n@pace.edu>
2022-03-25 15:07:52 -0400

    Update on AboutPage File & added descriptions

--- Welcome_Screen/app/src/main/java/com/pace/welcomescreen/SplashActivity.java
+++ Welcome_Screen/app/src/main/java/com/pace/welcomescreen/SplashActivity.java
@@ -10,7 +10,7 @@ import android.view.animation.Animation;
 import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity{
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
