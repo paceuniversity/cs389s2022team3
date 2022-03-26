068ba92a88269fdbd66049468bea585f649ab24e
jackegan444 <jack.egan444@gmail.com>
2022-03-26 14:40:13 -0400

    Add user profile class creation

--- app/src/main/java/com/pace/lumbar/ProfileCreation.java
+++ app/src/main/java/com/pace/lumbar/ProfileCreation.java
@@ -2,6 +2,7 @@ package com.pace.lumbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class ProfileCreation extends AppCompatActivity {
 
@@ -20,6 +22,7 @@ public class ProfileCreation extends AppCompatActivity {
     private Spinner stateSpinner;
     private Button profSelectBtn;
     private ImageView profImageView;
+    private Button createProfBtn;
     private int SELECT_PICTURE = 200;
 
     @Override
@@ -33,10 +36,12 @@ public class ProfileCreation extends AppCompatActivity {
         cityPt = findViewById(R.id.cityPlainText);
         profSelectBtn = findViewById(R.id.uploadImgBtn);
         profImageView = findViewById(R.id.profileImgView);
+        createProfBtn = findViewById(R.id.createProfBtn);
 
         //creates the state selection dropdown
         stateSpinner = findViewById(R.id.stateSpinner);
-        ArrayAdapter<CharSequence>adapter = ArrayAdapter.createFromResource(this, R.array.states, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence>adapter = ArrayAdapter.createFromResource
+                (this, R.array.states, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         stateSpinner.setAdapter(adapter);
 
@@ -48,10 +53,34 @@ public class ProfileCreation extends AppCompatActivity {
             }
         });
 
+        //pressing the create button will create a profile object
+        //TODO: store created users in database after Firebase tutorial
+        //TODO: exit out of profile creation after profile is saved
+        createProfBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isNotEmpty(namePt) && isNotEmpty(agePicker) &&
+                        isNotEmpty(cityPt) && stateSpinner.getSelectedItem() != null) {
+                    User newUser = new User(namePt.getText().toString(),
+                            Integer.parseInt(agePicker.getText().toString()),
+                            cityPt.getText().toString(), stateSpinner.getSelectedItem().toString());
+                } else{
+                    CharSequence incompleteMsg = "Creation failed: profile info incomplete";
+                    Toast.makeText(getApplicationContext(), incompleteMsg,
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
+
+    private boolean isNotEmpty(EditText edTxt) {
+        return edTxt.getText().toString().trim().length() > 0;
+    }
+
     //image selection code adapted from GeeksForGeeks:
     //https://www.geeksforgeeks.org/how-to-select-an-image-from-gallery-in-android/
-    void imageChooser() {
+    private void imageChooser() {
 
         // create an instance of the
         // intent of the type image
