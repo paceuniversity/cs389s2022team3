90e697f53065e6a50a31896ccdeea5b5a7685932
josephine chan <jc41405n@pace.edu>
2022-04-15 11:51:41 -0400

    Update on setting, profile, and chat page

---
+++ LumBar/app/src/main/java/com/pace/lumbar/ChatPage.java
@@ -0,0 +1,14 @@
+package com.pace.lumbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_page);
+    }
+}
--- LumBar/app/src/main/java/com/pace/lumbar/fragments/MatchesFragment.java
+++ LumBar/app/src/main/java/com/pace/lumbar/fragments/MatchesFragment.java
@@ -1,16 +1,29 @@
 package com.pace.lumbar.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import com.pace.lumbar.ChatPage;
 import com.pace.lumbar.R;
 
 
 public class MatchesFragment extends Fragment {
+
+    private TextView matchID, matchName;
+    private ImageView matchPic;
+    private MatchesFragment(View view){
+        view.setOnClickListener((View.OnClickListener) this);
+        matchID = (TextView) view.findViewById(R.id.matchID);
+        matchName = (TextView) view.findViewById(R.id.matchName);
+        matchPic = (ImageView) view.findViewById(R.id.matchPic);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,5 +35,13 @@ public class MatchesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_matches, container, false);
+    }
+
+    public void onClick (View view){
+        Intent intent = new Intent(view.getContext(), ChatPage.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("MatchID", matchID.getText().toString());
+        intent.putExtras(bundle);
+        view.getContext().startActivity(intent);
     }
 }
