c12ab4c5df44e24a2f5387cdc007fa2ba6167ef7
josephine chan <jc41405n@pace.edu>
2022-04-17 15:18:57 -0400

    added return button for help page

--- LumBar/app/src/main/java/com/pace/lumbar/HelpActivity.java
+++ LumBar/app/src/main/java/com/pace/lumbar/HelpActivity.java
@@ -2,13 +2,27 @@ package com.pace.lumbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HelpActivity extends AppCompatActivity {
+
+    private ImageButton revertBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        revertBtn = findViewById(R.id.revertHelp);
+        revertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HelpActivity.this, SettingPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
